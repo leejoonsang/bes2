954eb8ec313a0c5bb5470986b9cdfb464808eb3a
Sangjoon <djm04212@naver.com>
2022-10-06 15:34:46 +0900

    1글자씩 읽어오는 메서드 && 2글자씩 읽어오는 메서드

--- src/week3/day4/file/FileExercise.java
+++ src/week3/day4/file/FileExercise.java
@@ -1,9 +1,13 @@
 package week3.day4.file;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileExercise {
-    public static void main(String[] args) {
+
+    public void printFiles(){
         File dir = new File("./");
         File files[] = dir.listFiles();
 
@@ -11,15 +15,33 @@ public class FileExercise {
             System.out.println(file);
         }
     }
+
+    // 1글자씩 읽어오는 메서드
+    public char readAChar(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+        return (char)fileReader.read();
+    }
+
+    // 2글자씩 읽어오는 메서드
+    public String readTwoChars(String filename) throws IOException{
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        str += (char) fileReader.read();
+        str += (char) fileReader.read();
+        return str;
+    }
+    public static void main(String[] args) throws IOException {
+
+        FileExercise fileExercise = new FileExercise();
+        char c = fileExercise.readAChar("./a_file.txt");
+        String str = fileExercise.readTwoChars("./a_file.txt");
+        System.out.println(c);
+        System.out.println(str);
+
+    }
 }
 
 /*
-./.DS_Store
-./out
-./.gitignore
-./.git
-./a_file.txt
-./bes2.iml
-./.idea
-./src
+H
+He
  */
