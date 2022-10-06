9001be5afb5595c0b7ac043db29a79b175cf6893
Sangjoon <djm04212@naver.com>
2022-10-06 10:24:18 +0900

    Set 예제

---
+++ src/week3/day4/collection/SetExercise.java
@@ -0,0 +1,14 @@
+package week3.day4.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExercise {
+    public static void main(String[] args) {
+        Set<String> set1 = new HashSet<>();
+        set1.add("aaa");
+        set1.add("aaa");
+
+        System.out.println(set1);
+    }
+}
