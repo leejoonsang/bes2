1a3976aacf6dfa11b5019b384c3932c95eda7ce0
Sangjoon <djm04212@naver.com>
2022-10-07 01:48:54 +0900

    .

---
+++ src/week3/day4/collection/AlphabetCnt.java
@@ -0,0 +1,7 @@
+package week3.day4.collection;
+
+public class AlphabetCnt {
+    public static boolean isAlphabet(char alphabet) {
+        return (alphabet >= 'A' && alphabet <= 'Z') || (alphabet >= 'a' && alphabet <= 'z');
+    }
+}
