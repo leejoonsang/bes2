20bb3c1fc8749f4e295f52b87e09b9dde8dfef4d
Sangjoon <djm04212@naver.com>
2022-10-07 00:10:03 +0900

    텍스트 파일에서 1글자/2글자/n글자씩 읽어오기

--- src/week3/day4/file/FileExercise.java
+++ src/week3/day4/file/FileExercise.java
@@ -1,21 +1,19 @@
 package week3.day4.file;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileExercise {
 
     // 현재 디렉토리 파일 목록 출력
-    public void printFiles(){
-        File dir = new File("./");
-        File files[] = dir.listFiles();
-
-        for (File file : files) {
-            System.out.println(file);
-        }
-    }
+//    public void printFiles(){
+//        File dir = new File("./");
+//        File files[] = dir.listFiles();
+//
+//        for (File week3.day4.file : files) {
+//            System.out.println(week3.day4.file);
+//        }
+//    }
 
     // 1글자씩 읽어오는 메서드
     public char readAChar(String filename) throws IOException {
@@ -31,18 +29,35 @@ public class FileExercise {
         str += (char) fileReader.read();
         return str;
     }
+
+    // n글자씩 읽어오는 메소드
+    public String readNChars(String filename, int n) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+        int c = 0;
+        String str = "";
+        for (int i = 0; i < n; i++) {
+            if ((c = fileReader.read()) == -1)
+                break;
+            str += (char) c;
+        }
+        return str;
+    }
     public static void main(String[] args) throws IOException {
 
         FileExercise fileExercise = new FileExercise();
-        char c = fileExercise.readAChar("./a_file.txt");
-        String str = fileExercise.readTwoChars("./a_file.txt");
+
+        char c = fileExercise.readAChar("./b_file.txt");
+        String str = fileExercise.readTwoChars("./b_file.txt");
+        String str1 = fileExercise.readNChars("./b_file.txt", 15);
         System.out.println(c);
         System.out.println(str);
+        System.out.println(str1);
 
     }
 }
 
 /*
-H
-He
+F
+Fi
+File Reading Te
  */
