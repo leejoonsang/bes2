754cac2b59d84a096df4e1b554e48d5d1483a90f
Sangjoon <djm04212@naver.com>
2022-10-06 14:33:19 +0900

    한 바이트씩 읽는 메서드

---
+++ src/week3/day4/file/ReadFile.java
@@ -0,0 +1,32 @@
+package week3.day4.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+
+    private String filename;
+
+    public ReadFile(String filename) {
+        this.filename = filename;
+    }
+
+    char readOneByte (String filename) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024);
+        return (char)br.read();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        ReadFile readFile = new ReadFile("");
+        //char c = readFile.readOneByte();
+        File file = new File("./a_file.txt");
+        file.createNewFile();
+        System.out.println("파일 생성 되었는지?:" + file.exists());
+
+        System.out.println(readFile.readOneByte("./a_file.txt"));
+
+    }
+}
