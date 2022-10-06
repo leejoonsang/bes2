bb4e9cd744706c9909c30f99c69f70bc28141ec9
Sangjoon <djm04212@naver.com>
2022-10-06 13:46:47 +0900

    알파벳 세기

--- src/week3/day4/collection/AlphabetCntCheck.java
+++ src/week3/day4/collection/AlphabetCntCheck.java
@@ -1,18 +1,23 @@
 package week3.day4.collection;
 
 public class AlphabetCntCheck {
-    public boolean isAlphabet(String s) {
+    private String s1;
+    public AlphabetCntCheck(String s1) {
+        this.s1 = s1;
+    }
+    public void isAlphabet(String s) {
+
+        int cnt = 0;
 
         if(s == null){
-            return false;
+            System.out.println("문자열이 비었습니다.");
         }
         for(int i=0; i<s.length(); i++){
             char c1 = s.charAt(i);
-            if(!(c1 >= 'A' && c1 <= 'Z') && !(c1 >= 'a' && c1 <= 'z')){
-                return false;
+            if((c1 >= 'A' && c1 <= 'Z')) {
+                cnt++;
             }
-
         }
-        return true;
+        System.out.println(cnt);
     }
 }
--- src/week3/day4/collection/AlphabetCntMain.java
+++ src/week3/day4/collection/AlphabetCntMain.java
@@ -1,11 +1,19 @@
 package week3.day4.collection;
 
+import java.util.HashMap;
+
 public class AlphabetCntMain {
     public static void main(String[] args) {
-        String s1 = "aabbccddeeffgghhiijjkk";
-        for(int i=0; i<s1.length(); i++){
-            System.out.println(s1.charAt(i));
-        }
+        HashMap<String, Integer> alphabetMap = new HashMap<>();
+        String s1 = "aabb!@ccd$%^deef^^fg@*^ghh^(i(ijj".toUpperCase();
+
+        AlphabetCntCheck acc = new AlphabetCntCheck(s1);
+        acc.isAlphabet(s1);
+
+//        for(int i=0; i<s1.length(); i++){
+//            System.out.println(s1.charAt(i));
+//        }
+
     }
 
 }
