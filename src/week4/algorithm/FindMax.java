7bab6be252d5ae2c366c57a87901729e3dbe6c65
Sangjoon <djm04212@naver.com>
2022-10-12 11:38:20 +0900

    최대값 찾기 알고리즘

--- src/week4/algorithm/FindMax.java
+++ src/week4/algorithm/FindMax.java
@@ -7,23 +7,19 @@ public class FindMax {
         try{
             Scanner sc = new Scanner(System.in);
             int arr[] = new int[9];
-            int max = 0;
+            int maxValue = 0;
+            int maxIndex = 0;
 
             for(int i=0; i<arr.length; i++){
                 arr[i] = sc.nextInt();
-                if(arr[i] > max){
-                    max = arr[i];
+                if(arr[i] > maxValue){
+                    maxValue = arr[i];
+                    maxIndex = i;
                 }
             }
 
-            System.out.println(max);
-
-            for(int j=0; j<9; j++){
-                if(arr[j] == max){
-                    System.out.println(j+1);
-                    break;
-                }
-            }
+            System.out.println(maxValue);
+            System.out.println(maxIndex+1);
 
         }catch (Exception e){
             System.out.println("다시 입력하세요.");
--- src/week4/algorithm/FindMax2.java
+++ src/week4/algorithm/FindMax2.java
@@ -7,26 +7,22 @@ public class FindMax2 {
         try{
             Scanner sc = new Scanner(System.in);
             int[][] arr2 = new int[9][9];
-            int max = 0;
+            int maxValue = 0;
+            int[] maxIndex = {0, 0};
 
             for(int i=0; i<arr2.length; i++){
                 for(int j=0; j<arr2.length; j++){
                     arr2[i][j] = sc.nextInt();
-                    if(arr2[i][j] > max){
-                        max = arr2[i][j];
+                    if(arr2[i][j] > maxValue){
+                        maxValue = arr2[i][j];
+                        maxIndex[0] = i;
+                        maxIndex[1] = j;
                     }
                 }
             }
 
-            System.out.println(max);
-
-            for(int i=0; i<arr2.length; i++){
-                for(int j=0; j<arr2.length; j++){
-                    if(arr2[i][j] == max){
-                        System.out.println((i+1) + " " + (j+1));
-                    }
-                }
-            }
+            System.out.println(maxValue);
+            System.out.println((maxIndex[0]+1) + " " + (maxIndex[1]+1));
 
         }catch (Exception e){
             System.out.println("다시 입력하세요.");
