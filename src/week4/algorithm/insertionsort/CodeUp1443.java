cdbf66278e65c366c63d8ba0d04851980e2971bb
Sangjoon <djm04212@naver.com>
2022-10-14 10:52:16 +0900

    Insertion Sort codeUp #1443 - refactoring

--- src/week4/algorithm/insertionsort/CodeUp1443.java
+++ src/week4/algorithm/insertionsort/CodeUp1443.java
@@ -4,29 +4,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class CodeUp1443 {
+
+    public int[] sorting(int arr[]){
+        for(int i=1; i<arr.length; i++) {
+            for(int j=i; j>0; j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
+        CodeUp1443 is = new CodeUp1443();
+
         try{
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             int[] arr = new int[n];
+            int result[];
 
             for(int i=0; i<n; i++){
                 arr[i] = sc.nextInt();
             }
 
-            for(int i=1; i<arr.length; i++){
-                for(int j=i; j>0; j--){
-                    if(arr[j] < arr[j-1]){
-                        int temp = arr[j];
-                        arr[j] = arr[j-1];
-                        arr[j-1] = temp;
-                    }
-                }
-            }
-
-            for(int element : arr) {
-                System.out.println(element);
-            }
+            result = is.sorting(arr);
+            System.out.println(Arrays.toString(result));
 
         }catch (Exception e){
             System.out.println("다시 입력하세요.");
