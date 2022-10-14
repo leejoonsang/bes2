f063785be4623e01d5a92345f039030664823af9
Sangjoon <djm04212@naver.com>
2022-10-14 09:49:14 +0900

    Insertion Sort - swap

--- src/week4/algorithm/insertionsort/InsertionSort01.java
+++ src/week4/algorithm/insertionsort/InsertionSort01.java
@@ -6,13 +6,20 @@ public class InsertionSort01 {
 
     public int[] sort(int[] arr){
         int i=1;
+        int temp;
+        if(arr[i] < arr[i-1]){
+            // swap
+            temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+        }
 
-        return new int[]{};
+        return arr;
     }
 
     public static void main(String[] args) {
         // 5에서 출발
-        int[] arr = {8, 5, 6, 2, 4};
+        int[] arr = new int[]{8, 5, 6, 2, 4};
 
         InsertionSort01 insertionSort01 = new InsertionSort01();
         int result[] = insertionSort01.sort(arr);
