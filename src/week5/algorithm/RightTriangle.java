4c77d330c388e8d290b52066c30afdf333d16dee
Sangjoon <djm04212@naver.com>
2022-10-17 09:10:30 +0900

    별찍기 - 직각삼각형 step1

---
+++ src/week5/algorithm/RightTriangle.java
@@ -0,0 +1,10 @@
+package week5.algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
