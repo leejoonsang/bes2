f833b539561094a73738e5570b1c908b4d948ac5
Sangjoon <djm04212@naver.com>
2022-10-17 09:23:55 +0900

    별찍기 - 직각삼각형 step2

--- src/homework/codeup/CodeUp1022.java
+++ src/homework/codeup/CodeUp1022.java
@@ -1,7 +1,18 @@
 package homework.codeup;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class CodeUp1022 {
     public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str = br.readLine();
 
+            System.out.println(str);
+
+        }catch (Exception e){
+            System.out.println("다시 입력하세요.");
+        }
     }
 }
