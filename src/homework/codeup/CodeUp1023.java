edb4a4ae3dd4f3ba8a549c7959d7dedec6c734b8
Sangjoon <djm04212@naver.com>
2022-10-17 09:26:41 +0900

    codeUp 과제

---
+++ src/homework/codeup/CodeUp1023.java
@@ -0,0 +1,24 @@
+package homework.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1023 {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+            float num = sc.nextFloat();
+            String numStr = Float.toString(num);
+
+            String[] intDecimalPart = numStr.split("\\.");
+            String numIntPart = intDecimalPart[0];
+            String numDecimalPart = intDecimalPart[1];
+
+            System.out.println(numIntPart);
+            System.out.println(numDecimalPart);
+
+        }catch (Exception e){
+            System.out.println("다시 입력하세요.");
+            e.printStackTrace();
+        }
+    }
+}
