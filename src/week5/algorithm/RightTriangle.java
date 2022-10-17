d076ac66a50a6d6667751094b017605d0cc2a57b
Sangjoon <djm04212@naver.com>
2022-10-17 09:24:56 +0900

    별찍기 - 직각삼각형 step2

--- src/week5/algorithm/RightTriangle.java
+++ src/week5/algorithm/RightTriangle.java
@@ -1,10 +1,28 @@
 package week5.algorithm;
 
+import java.util.Scanner;
+
 public class RightTriangle {
+
+    public void printAsterisk(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+        try{
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            RightTriangle rightTriangle = new RightTriangle();
+            rightTriangle.printAsterisk(n);
+
+        }catch (Exception e){
+            System.out.println("다시 입력하세요.");
+        }
     }
 }
